c5d0b5a161a6dd07d765adff256da6ea01640dc7
joncastro <castro.jon@gmail.com>
2019-08-24 12:50:39 +0200

    allow create a Command with a given template. Add methods to push the template

--- pytijo_api_client/command.py
+++ pytijo_api_client/command.py
@@ -14,6 +14,15 @@ ALL_COMMAND_LRU = LRUCache(capacity=1000)
 
 DEFAULT_HEADERS = {"content-type": "application/json", "accept": "application/json"}
 
+DEFAULT_FACTS = [
+    "system",
+    "kernel",
+    "machine",
+    "python_version",
+    "command_path",
+    "command_path_filesize",
+]
+
 
 class Command:
     def __init__(
@@ -24,12 +33,13 @@ class Command:
         tijo_api=DEFAULT_TIJO_API,
         insecure=False,
         timeout=60,
+        template=None,
     ):
         if len(args) == 0 or args[0] is None or len(args[0]) == 0:
             raise AttributeError(
                 "arguments must be a list of strings and should contain at least one value"
             )
-        self.template = None
+        self.template = template
         self.cache_folder = os.path.expanduser(cache_folder) if cache_folder else None
         self.lru_capacity = lru_capacity
         self.args = args
@@ -78,18 +88,58 @@ class Command:
                 command_templates.save()
         return self.template
 
-    def _find_template(self, offset=0, limit=1):
+    def push_template(self, name=None, tags=None, facts=None, facts_attributes=None):
         data = {
             "command": self.command_basename,
-            "command_facts": {
-                "system": self.system,
-                "kernel": self.kernel,
-                "machine": self.machine,
-                "python_version": self.python_version,
-                "command_path": self.command_path,
-                "command_path_filesize": self.command_path_filesize,
-            },
+            "name": name if name is not None else self.command_basename,
+            "json": self.template,
         }
+        if tags is not None and isinstance(tags, list) and len(tags) > 0:
+            data["tags"] = " ".join(tags)
+        elif tags is not None:
+            data["tags"] = tags
+
+        # calculate the facts
+        default_facts = self._get_facts(facts_attributes)
+        default_facts = {} if default_facts is None else default_facts
+        if facts is not None and len(facts) > 0:
+            for key in facts:
+                default_facts[key] = facts[key]
+
+        if default_facts is not None and len(default_facts) > 0:
+            data["command_facts"] = default_facts
+
+        resp = requests.post(
+            "{}/templates".format(self.tijo_api),
+            verify=not self.insecure,
+            timeout=self.timeout,
+            headers=DEFAULT_HEADERS,
+            data=json.dumps(data),
+        )
+
+        if resp is None or (resp.status_code != 200 and resp.status_code != 201):
+            return None
+        try:
+            return json.loads(resp.content.decode("utf-8"))
+        except ValueError:
+            return None
+        return None
+
+    def _get_facts(self, attributes=DEFAULT_FACTS):
+        if attributes is None or len(attributes) == 0:
+            return None
+        facts = {}
+        for attribute in attributes:
+            if hasattr(self, attribute) and getattr(self, attribute):
+                facts[attribute] = getattr(self, attribute)
+        return facts if len(facts) > 0 else None
+
+    def _find_template(self, offset=0, limit=1):
+        data = {"command": self.command_basename}
+        facts = self._get_facts()
+        if facts is not None and len(facts) > 0:
+            data["command_facts"] = facts
+
         if self.command_args and len(self.command_args) > 0:
             data["command_args"] = " ".join(self.command_args)
 
